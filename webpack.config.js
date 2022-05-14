9186ac041136f897f013f2defc4a7984ea72786f
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-05-14 18:43:49 +0400

    set up config for React (babel,presets,extentions)

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,39 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: ["@babel/polyfill",  "./src/index.jsx"],
     output: {
         filename: "[name].[hash].js",
         path: path.resolve(__dirname, 'webpackDist'),
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.jsx'],
     },
     devServer: {
         port: 3000
     },
     plugins: [
         new HtmlWebpackPlugin({template: "./src/index.html"}),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
     ],
     module: {
         rules: [
             {test: /\.(css|less)$/, use: ['style-loader', 'css-loader', 'less-loader']},
-            {test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource',}
+            {test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource',},
+            {
+                test: /\.m?js$/, exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {presets: ['@babel/preset-env']}
+                }
+            },
+            {
+                test: /\.m?jsx$/, exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {presets: ['@babel/preset-env', '@babel/preset-react']}
+                }
+            }
         ]
     }
 }
