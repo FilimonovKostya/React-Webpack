c851b07d152d61b9629c805d3999d286bbe98f3d
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-05-14 18:14:45 +0400

    was added image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import './index.less'
+import image from './assets/image.jpg'
 
-export function MyFunc(user){
+export function MyFunc(user) {
     console.log(`Hi ${user}`)
+    return 'success'
 }
+
+console.log(MyFunc('Kostya'))
+
+console.log('img --->', image)
+
+// Add image to tag
+let img = document.createElement("img");
+img.src = image;
+let src = document.getElementById("image");
+src.appendChild(img);
+img.style.width = '700px'
+img.style.height = '350px'
